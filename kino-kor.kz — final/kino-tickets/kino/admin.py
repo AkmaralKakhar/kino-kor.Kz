3d70a246c7ff9758cd2a7da5f536d5a7da78bf30
AkmaralKakhar <55820844+AkmaralKakhar@users.noreply.github.com>
2021-12-23 15:30:02 +0600

    Add files via upload

---
+++ kino-kor.kz — final/kino-tickets/kino/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+from  embed_video.admin  import  AdminVideoMixin
+
+
+admin.site.register(Session)
+admin.site.register(News)
+
+
+class  video(AdminVideoMixin, admin.ModelAdmin):
+	pass
+
+admin.site.register(Film)
